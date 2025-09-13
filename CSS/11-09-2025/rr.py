8aefd8c9b41b0bbff9d2f7318d83f7718bf90796
HasanCoder66 <hasanashraf874gmail.com>
2025-09-13 13:06:58 +0500

    revision

---
+++ CSS/11-09-2025/rr.py
@@ -0,0 +1,16 @@
+user = int(input("tell me your age: "))
+if user >= 18:
+    user_gen = input("apna gender batao").lower()
+    if user_gen == "male":
+        print("vote de sakte ho bare ho gae ho:")
+else:
+    print("baap ko bhej tere bas k baat nhi :")
+    male
+    Male
+    mAle
+
+a = int(input("a"))
+b = int(input("b"))
+c = int(input("c"))
+
+var = -b-((b**2)-4*a*c)**0.5/2*a
