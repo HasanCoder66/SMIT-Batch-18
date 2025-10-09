82cb8354ffcdd815f572779224bbbc116d5e6fd8
HasanCoder66 <hasanashraf874@gmail.com>
2025-10-09 12:54:09 +0500

    js ch 1 to 3

---
+++ JavaScript/09-10-2025/app.js
@@ -0,0 +1,113 @@
+// Js Mehtods
+
+// console.log()
+
+// alert("Aapny ye kaam nhi kerna")
+
+// document.writeln("<h1>Hello World </h1> <br> <p> lorem12313456 </p>")
+
+// word or keyword
+
+// keyword kia hota hai ?
+
+// esy words jo js mai already defined hon or unki koi special meaning ho ya unsy koi kaam kerasekty hon
+
+// hasan
+
+// Js Keywords
+// if , else, alert, else if, switch, loop, const, var, let, prompt, function , delete, remove
+
+// for variable creation ==>
+// var , let & const
+
+// variable banany ka syntax asaan bhasa mai
+// keyword  , variableName, Assigning Operator ya equals , Value
+
+// let a = "12";
+
+// console.log(a)
+
+// what is alert ?
+// alert 1 js ka method hai
+
+// method kia hai ?
+
+// value kia hai ?
+
+// 12               ==> Data Type Number
+// true false       ==> Data Type Boolean
+// "string"         ==> Data Type String
+// undefined         ==> Data Type Undefined
+
+// let user = false ;
+
+// let user = "hasan"
+// console.log("yaha pr mujhy user mil raha hai",user)
+
+// console.log("first")
+// alert("Hello World")
+// window.alert("Hello World")
+
+// var username = "hasan";
+// var userDesc = "My Name is ===>";
+// console.log("Username ==>",username)
+// console.log("User Desc ==>",userDesc)
+
+// let num = 12;
+// let num2 = 15;
+
+// let result = num + num2
+
+
+// let num;       // variable declaration
+// let num = 12;       // variable declaration & initialization
+// console.log(num)
+
+// num = 15;        //reassign
+
+// console.log("sum of 2 numbers ==>",result);
+
+
+// let firstName = "Muhammad"
+// let lastName = "Hasan"
+
+// let fullName = firstName + lastName
+// console.log("Full Name ==> ", fullName)
+
+
+
+
+// Assignment JavaScript 
+
+// Chapter 1 
+
+// Question 1 
+
+// Answer Code ==>
+
+    // alert("Hello User! \n kia haal hain")
+
+
+
+
+
+
+    // Question 2
+
+// Answer Code ==>
+
+    // alert("Hello User! \n kia haal hain")
+
+
+
+
+
+
+
+
+
+
+    // Chapter 2
+
+    var userName = "Hasan"
+    
