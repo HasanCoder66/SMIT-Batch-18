14159ccbc962399cc6a7f21f6485d75932f8ac4d
HasanCoder66 <hasanashraf874@gmail.com>
2025-10-14 13:12:29 +0500

    ch 4 & 5 slides 1 to 9 done

---
+++ JavaScript/14-10-2025/app.js
@@ -0,0 +1,69 @@
+console.log("Js Running ==>")
+
+/* document.writeln(`<h1> Hello World </h1>`)
+
+*/
+
+// var firstName = "hasan"
+// var firstName = 'hasan'
+
+// TEMPLATE LITERALS ==>
+// var firstName = `hasan`
+
+
+
+
+// let message = "Hello"
+
+
+
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+
+
+// Illegal Variable Name ===>
+// var, delete, function, 1st, !username
+
+// variable name hamesha numbers or special charcter sai start nhi hoty 
+
+// siwaye 2 special character ke  -->
+// wo kon sai hain ? $ _
+
+// let username1 = "Muhammad Hasan"
+
+
+
+
+// case senitive  ?
+
+// var FULLNAME = "MUHAMMAD HASAN"
+
+// console.log(FULLNAME)
+
+
+// pascal case  --> FULLNAME
+// snake case   --> full_name
+// camel case   --> fullName
+// kebab case   --> full-name
+
+// var num1 = 12;
+
+
+
+var num1 = 10;
+// var num2 = 2;
+
+// Familiar Operators  -->  +, -, *, /, %
+// console.log(num1 % num2)
+
+
+
+// UnFamiliar Operator  ---> 
+
+
+num1++
+console.log(num1)
+
---
+++ JavaScript/Problem Solved/app.js
@@ -0,0 +1,17 @@
+// console.log("Hello World!")
+
+// Problem 1  -->
+
+// var, let, const 
+
+let firstName = "Muhammad" 
+let lastName = "Hasan"
+
+let address = "main street"
+
+address = "Lorem IPsum"
+
+
+console.log("First Name -->",firstName)
+console.log("Last Name -->",lastName)
+console.log("Address -->",  address)
