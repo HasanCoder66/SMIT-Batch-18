648fb86a7c9cac3c6d260d15dd1520c1e4111f75
hasanCoder66 <hasanashraf874@gmail.com>
2025-10-16 13:07:02 +0500

    Revision & Ch 6 to 9

---
+++ JavaScript/16-10-2025/app.js
@@ -0,0 +1,75 @@
+// console.log("Javascript Running --->")
+
+
+// alert("Hello World")
+
+
+// 2 tarha ki data types hoti hai 
+
+// 1 primitive   --> string, undefined, number, null, boolean, symbol, bigint
+// 2 reference         (), [], {}
+
+
+
+
+
+
+// var username; // ----> declare
+
+// console.log(username) // -->  undefined
+
+
+// username = "hasan"
+
+// console.log("username -->", username)  //---> hasan
+
+// var twelve = 12;
+
+
+
+// var num = twelve;
+
+
+
+// console.log(num)
+
+
+let firstName = "Muhammad"
+let lastName = "Hasan"
+
+
+// let space = "";
+// console.log(space)
+let fullName = firstName +  " " + lastName;
+console.log("fullName -->", fullName)
+
+
+
+// + 
+
+// var num1 = 12 ;
+// var num2 = "a";
+
+// console.log(num1 - num2)
+
+var num = 2;
+// console.log(--num)
+// console.log(num--)
+// console.log(num)
+var result = num--;
+
+// console.log(typeof result)
+
+// console.log(num--)   //1
+
+
+//   //0 
+// console.log(++num)  //1 
+// console.log(num--) 
+
+
+// let userName = prompt("Enter Your Name")
+// console.log("UserName ==>" , userName)
+
+// let userFullName = userName + "Ashraf"
+// console.log(userFullName)
