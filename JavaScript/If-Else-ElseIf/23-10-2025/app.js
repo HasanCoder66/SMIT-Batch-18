66c78b9d8e906dec1be4662a58791ab0f06520fc
hasanCoder66 <hasanashraf874@gmail.com>
2025-10-23 13:10:43 +0500

    If Else & Else If Revision || Problem Solved Pdf Questions

---
+++ JavaScript/If-Else-ElseIf/23-10-2025/app.js
@@ -0,0 +1,133 @@
+// console.log("JavaScript --> Running")
+
+// let lang = "c++"
+// if(lang == "javascript"){  /// condition true
+// // some code here
+// console.log("if chl raha hai ==>")
+// //execution
+
+
+// }else if (lang == "python") {
+//     console.log("else if chl raha hai -")
+// }
+
+// else {
+//     console.log("else chl raha hai ==>")
+// }
+
+
+
+
+
+// let khanaKhanaHai = true;
+
+
+// if (khanaKhanaHai) {
+//     let kiaKhanaHai = prompt("Kia Khana hai")
+
+//     console.log("kiaKhanaHai  --->",kiaKhanaHai)
+
+//     if(kiaKhanaHai === "Biryani") {
+//         console.log("250 Rupy lagy gy")
+//     }else if (kiaKhanaHai === "Sada Burger"){
+//         console.log("100 Rupy lagy gy")
+//     }else {
+//         console.log("Aye ku ho")
+//     }
+// }
+
+
+
+
+
+// let money = prompt("pesy kitny hain ?");
+
+// if (money >= 200){
+// console.log("Sadi Biryani khalooo")
+// }else if (money < 200 && money >= 100){
+//     console.log("Haleem or 2 Rooti khila do")
+// }else if (money >= 300){
+//     console.log("Chicken Biryani khalooo")
+// }else {
+//     console.log("Free mai khana hai -->")
+// }
+
+
+
+
+
+
+
+// let num = "enter";
+
+// console.log(num.charCodeAt(0))
+
+
+
+// Guess Number Game
+
+let secretNum = 4;
+
+let userNum = 7;
+
+
+// if (secretNum == userNum) {
+//     console.log("Correct Guess")
+// } else if (userNum == secretNum - 1 || userNum == secretNum + 1) {
+//     console.log("Bas Ek Step door")
+// } else if (userNum == secretNum - 2 || userNum == secretNum + 2) {
+//     console.log("Kuch or try kero")
+// } else if (userNum == secretNum - 3 || userNum == secretNum + 3) {
+//     console.log("bohat low guess hai")
+// }
+// else {
+//     console.log("Try Again")
+// }
+
+
+
+// let firstNum = 12;
+// let secNum = 2
+
+// let operation = "/"
+
+
+// if (operation == "+") {
+//     console.log(firstNum + secNum)
+// } else if (operation == "/") {
+//     console.log(firstNum / secNum)
+// }
+
+// else if (operation == "-") {
+//     console.log(firstNum - secNum)
+// }
+
+// else {
+//     console.log("Invalid Operator")
+// }
+
+
+
+// let str = "a"
+
+// console.log(str.charCodeAt())
+// if(str.charCodeAt()  > 65 && str.charCodeAt() <= 90){
+//     console.log("It is a Capital Alphabet")
+// }else if (str.charCodeAt() >= 97 && str.charCodeAt() < 123) {
+//     console.log("It is a Small Alphabet")
+// }
+
+
+
+
+
+let time = 1199;
+
+
+if(time == 1900){
+    console.log("7PM")
+} else if (time >= 0 && time < 1200) {
+console.log("Good Morning")
+}  else if (time >= 1200 && time < 1700) {
+console.log("Good Afternoon")
+}
