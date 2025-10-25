61472f532faef43d000f8fbb07865d9f2e70f3d9
HasanCoder66 <hasanashraf874@gmail.com>
2025-10-25 12:58:10 +0500

    ATM Machine Logic builing

---
+++ JavaScript/If-Else-ElseIf/25-10-2025/app.js
@@ -0,0 +1,49 @@
+// console.log("javascript chl rahi hai -->")
+
+
+
+
+let atmMachine = 50000;
+let salary = 25000;
+
+// let withdraw = 52000;
+
+
+// if(salary > withdraw){
+//     salary = salary - withdraw
+//     atmMachine = atmMachine - withdraw
+//     console.log(atmMachine)
+// } else if (salary < withdraw && atmMachine > withdraw){
+//     console.log("aap jo pesy mang wo ziada")
+// }else if (atmMachine < withdraw ) {
+//     console.log("itny pesy nhi hain hbl ke pass")
+// }
+
+
+
+
+
+
+
+
+
+
+// user 
+
+alert ("Welcome to ATM Machine \nHello User!")
+let pinCode = 45445;
+let userPinCode = prompt("Enter your pin code")
+
+if(userPinCode == pinCode){
+// console.log("agy barha do")
+let pesyNikalnyHain = "yes"
+let kiaPesyNikalnyAyeHo = prompt("Enter Yes Or No")
+if(pesyNikalnyHain == kiaPesyNikalnyAyeHo) {
+let withdraw = prompt("Enter Your Withdraw Amount");
+console.log(withdraw)
+}else {
+    console.log("to kiu aye ho phir")
+}
+}
+
+
