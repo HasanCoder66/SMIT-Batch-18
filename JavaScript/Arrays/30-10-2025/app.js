1439d0ef50241b499df5634db332e4d8b2d7f014
hasanCoder66 <hasanashraf874@gmai.com>
2025-10-30 13:02:19 +0500

    Revision Array, Array Methods --> push, pop, shift, unshift, slice, splice

---
+++ JavaScript/Arrays/30-10-2025/app.js
@@ -0,0 +1,77 @@
+// console.log("Javascript is running --->")
+
+
+// let citiesArr = []
+// citiesArr[citiesArr.length]= "Lahore"
+// citiesArr[citiesArr.length ]= "Karachi"
+// console.log(citiesArr)
+// console.log(`Welcome to ${citiesArr[2]}`)
+
+
+// let mixedArr = [12, true, false, [], "abdul rehman", undefined, null, {}]
+
+// console.log(mixedArr)
+
+
+
+// let citiesArr = [12, true, false,]
+
+// let returnVal = citiesArr.shift()
+
+// // citiesArr.unshift("Lahore")
+
+
+// console.log(returnVal)
+
+// console.log(citiesArr)
+
+
+
+
+
+
+// let studentsArr = ["Abdullah", "Asad", "Hasnain", "Irfan"];
+// console.log(studentsArr)
+
+// studentsArr.splice(1,2)  // delete
+// studentsArr.splice(1,2, "Hasan", "Bilal", "Sarim")  // update
+
+// let returnVal = studentsArr.splice(2,2, "Maqsood")
+// console.log("Remove hoky arry mil raha -->",studentsArr)
+
+// console.log("return splice ka -->",returnVal)
+// console.log(studentsArr)
+
+
+
+
+
+
+// let studentsArr = ["Abdullah", "Asad", "Hasnain", "Irfan"];
+
+// let sliceReturn = studentsArr.slice(1,3)
+
+// console.log(sliceReturn)
+
+// console.log("Students Array --> ",studentsArr)
+
+
+
+
+// let studentArr = ["abdullah", "hasan", "Adeel"]
+
+// let returnValue = studentArr.shift();
+
+
+// console.log(returnValue)
+
+
+// let newArr = new Array()
+
+// console.log(newArr)
+
+
+// newArr.unshift("Ismail")
+// console.log(newArr)
+
+
