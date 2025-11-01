9cf8304d2384fa6677d4df788ce4ef738ec90cf8
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-01 13:03:58 +0500

    for loop and Revision flag boolean array length

---
+++ JavaScript/Arrays/1-11-2025/app.js
@@ -0,0 +1,52 @@
+// console.log("Running -->")
+
+
+// let arr = [["hasan", true, "Ismail"],[],[]]
+// console.log(arr[0][arr[0].length -1])
+
+
+// arr[0][arr[0].length -1] = "Asad"
+
+
+// console.log(arr)
+
+
+
+let arr = ["asldkflasjk",4654,789510,45]
+
+
+let apkoAddKerna = "Yellow"
+
+arr.push(apkoAddKerna)
+
+
+// console.log(arr.length -1)
+
+
+
+// let randomNum = Math.floor(Math.random() * 4) 
+
+// console.log(randomNum)
+
+
+// console.log(arr[randomNum])
+
+
+
+
+
+
+
+
+
+
+
+
+// return -->
+
+let arr2 = [12]
+
+
+let returnVal = arr2.shift()
+
+console.log(returnVal)
---
+++ JavaScript/For Loop/app.js
@@ -0,0 +1,71 @@
+// loop kisy kehtry 
+// kisi bi kaam ko baar baar repeat kerny ko loop kehty 
+
+
+
+// start ; end;  change;
+// variable initialize , condition , chnages
+
+// for (let i = 1; i < 2; i++) {
+// console.log("Test",i)
+// }
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson",  "Great Falls","Karachi", "Honolulu", ];
+
+// console.log(cleanestCities[0])
+// console.log(cleanestCities.length)
+
+// var cityToCheck = "Karachi"
+
+
+// let flag = "no"
+
+// let flag = true; 
+// for (let i = 0; i < cleanestCities.length; i++){
+//     // console.log(cleanestCities[i])
+//     if(cityToCheck === cleanestCities[i]){
+//         flag = true
+//         console.log("Karachi cleanest city hai ")
+//         break;
+// }
+// else {
+//     console.log("nhi mila")
+// }
+//     // console.log(i)
+// }
+
+// console.log(flag)
+
+
+// if(flag){
+//     console.log("Milgaya")
+// }else {
+//     console.log("nhi mila")
+// }
+
+
+
+// for (let i = 1; i <= 10; ++i){
+//     // break
+//     console.log(i)
+// }
+
+
+
+
+// let tableNum = 2;
+// for (let i = 1; i < 11; i++){
+//     if(i == 5){
+
+//         break
+//     }
+//     console.log(tableNum, "X",i, "=", tableNum * i)
+// }
+
+
+
+
+// 10
+// 9
+// 8
+// 7
