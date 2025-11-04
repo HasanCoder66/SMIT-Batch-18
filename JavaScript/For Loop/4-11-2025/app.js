69d02f7bec9791573850ad3ffa2bf63bbc87c211
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-04 13:15:55 +0500

    loops and nested loop and js chapter 23 done

---
+++ JavaScript/For Loop/4-11-2025/app.js
@@ -0,0 +1,155 @@
+
+// start variable initilize , condition , increment or decrement 
+
+// start , end, change 
+
+
+// let cities = ["karachi", "lahore", "muree"]
+// // console.log()
+// for (let i = 0; i < cities.length; i++) {
+//     console.log(cities[i])
+// }
+
+
+
+// find max number in array 
+
+let arrNum = [1,2,3,4,5,6]
+
+let maxNum = arrNum[0]
+
+// console.log(maxNum)
+
+
+// for(let i =0 ; i < arrNum.length; i++){
+//     console.log(arrNum[i])
+//     if(maxNum > arrNum[i]){
+//         maxNum = arrNum[i]
+//         // console.log("mil gaya")
+//     }
+// }
+
+
+// if(maxNum){
+// console.log("Min Number -->",maxNum)
+// }else {
+//     console.log("max num nhi hai")
+// }
+
+// let num=[100,1,2,3,4,5,10];
+// let max= num[2];
+// for (let i = 0; i < num.length; i++) {
+//     if (max < num[i]) {
+//         max=num[i];
+//         console.log(num[i])
+//     }
+    
+// }
+// console.log(max)
+
+
+let arrData = [
+    [0,0,1,0],
+     [1,0,1 ], 
+     [true, false]
+    ]
+
+    // console.log(arrData[0][0])
+
+
+// for(let i =0; i < arrData.length; i++){
+//     // console.log(arrData[i])
+// for(let j =0; j < arrData[i].length; j++){
+//     // console.log(i,j)
+//     console.log(arrData[i][j])
+
+
+// }
+
+// }
+
+
+var cleanestCities = ["karachi", "cheyenne", "Santa Fe", "santa fe", "Tucson",];
+
+// console.log(cleanestCities[0].toLowerCase())
+
+
+// if(cleanestCities[0] == "karachi"){
+//     console.log("han milgaya karachi")
+// }
+// else {  
+//     console.log("nhi mila karachi")
+// }
+
+
+// for(let i = 0; i < cleanestCities.length; i++){
+//     console.log(cleanestCities[i])
+//     if(cleanestCities[i].toUpperCase() == "KARACHI"){
+//         console.log("han bhai milgaya")
+//         break;
+//     }else {
+//         console.log("nhi mil raha ")
+//     }
+// }
+
+
+
+
+
+// let firstName = "hasan"
+// let lastName = "Ashraf"
+
+// let firstChar = firstName[0].toUpperCase()
+
+// let fullName = firstChar + firstName.slice(1) +  " " +  lastName
+// console.log(fullName)
+
+
+// console.log(firstName.length)
+
+
+
+
+
+
+
+let paragraph = " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem facilis aspernatur."
+
+console.log(paragraph.length)
+
+
+console.log(paragraph.lastIndexOf("enim"))
+
+// let intro = "My Name is Hasan."
+// console.log(intro.length)
+
+// let myName = ""
+// let flag = false;
+// for(let i = 0; i < intro.length; i++){
+//     console.log(intro[i])
+//     if(intro.slice(i, i + 5) == "Hasan"){
+// flag = true
+// myName = "Hasan"
+//     }
+//     // console.log(intro.slice(i, i + 5))
+
+// }
+// console.log("My Name -->",myName)
+// if(flag) {
+// console.log("mil gaya")
+// }else {
+//     console.log("nhi mila")
+// }
+
+
+
+
+
+// const a = [12];
+
+// a[0] = "hasan"
+//  console.log(a)
+
+
+
+
