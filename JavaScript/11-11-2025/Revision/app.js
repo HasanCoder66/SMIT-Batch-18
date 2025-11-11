3d7fa45203f9191321320f9c19fcdfe60b295943
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-11 12:58:13 +0500

    js chapter 30 done

---
+++ JavaScript/11-11-2025/Revision/app.js
@@ -0,0 +1,78 @@
+// console.log("js running -->")
+
+// let para = "my name is hasan"
+
+// let returnVal = para.split(" ")
+
+// for(let i = 0 ; i < returnVal.length ; i++){
+//     // console.log()
+//     let upperCase = returnVal[i][0].toUpperCase()
+//     let removeFirstChar = returnVal[i].slice(1)
+//     let updatedValue = " " + upperCase  + removeFirstChar
+//     // console.log(updatedValue)
+
+//     para += updatedValue
+
+// }
+
+// let newPara =para.split(" ").slice(4).join(" ")
+// console.log(newPara)
+
+
+
+
+
+let para = "hasan my name is hasan"
+// // console.log(para.lastIndexOf("hasan"))
+// let userName = ""
+
+// for(let i = 0 ; i < para.length; i++){
+//     console.log(para[i])
+
+//     if(para.slice(i, i + 5) == "hasan"){
+//         userName = "ahsan"
+//         break
+//     }
+//     // console.log()
+
+// }
+
+// console.log(userName)
+
+
+console.log(para.replaceAll("hasan", "abdullah"))
+// console.log(para.replace(/hasan/g, "abdullah"))
+
+
+let num = 5;
+
+// console.log(Math.round(num))
+// console.log(Math.ceil(num))
+// console.log(Math.floor(num))
+
+// let arr = [true, false, null, undefined, [], {}, alert]
+
+
+// console.log(Math.round(Math.random() * arr.length))
+
+
+
+
+// var currentAge = prompt("Enter your age.");
+
+// var yearsEligibleToVote = currentAge - 18;
+
+
+
+let str = 12
+// console.log(typeof Number(str))
+
+
+// console.log(parseInt(str))
+// console.log(parseFloat(str))
+
+// console.log(String(str))
+
+console.log(str.toFixed(4))
+
+
