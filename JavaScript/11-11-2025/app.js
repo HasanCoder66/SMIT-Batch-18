736446d1c96526660b14f97069c89ffef437abcf
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-11 10:58:42 +0500

    arrays and arrays methods

---
+++ JavaScript/11-11-2025/app.js
@@ -0,0 +1,98 @@
+console.log("js running -->")
+
+
+// let a = 2;
+
+// ++a
+// console.log(++a + a - a)
+
+// console.log(a)
+
+
+// if(false){
+// console.log("chala do")
+// }else {
+// console.log("else chl raha hai")
+// }
+
+
+
+
+
+// let age ;
+
+// console.log(age)
+
+
+// if(age){
+//     console.log("if chl raha ")
+// }else {
+//     console.log("undefined mil raha ")
+// }
+
+
+
+// falsy values in javascript
+// 0, "", false, undefined, null, NaN
+
+// let student1 = "Hasan"
+
+
+// // {}, [], ()
+
+
+// // let studentsArr = ["hasan", true, [],undefined, {}, alert,"", "laskdfj", "lkasjdfl",12, 78, 89, 45, 8, 786]
+
+// var user = "hasan"
+
+// var studentsArr = []
+
+// // console.log(studentsArr.push(user))
+
+// var returnVal = studentsArr.push(user, "ahsan", "huzaifa", "shakeel")
+
+// console.log(returnVal)
+
+// console.log(studentsArr)
+// // console.log("ye mil raha hai last index pr ---> " + studentsArr[studentsArr.length -1])
+
+
+// // console.log(age)
+
+
+// studentsArr[0] = "umair"
+
+
+// console.log(studentsArr)
+
+
+let cities = ["karachi", "Lahore","Islamabad"]
+// let konsiAddKerniHai = "Islamabad"
+
+// let returnVal = cities.push(konsiAddKerniHai)
+// let returnVal = cities.unshift(konsiAddKerniHai)
+// let returnVal = cities.pop()
+// let returnVal = cities.shift()
+// console.log("return value --> ", returnVal)
+
+// target index  , kitny remove kerny hain,  konsay add kerny hain
+// update --> cities.splice(1,1, "Islamabad", "Multan")
+// Add -->  cities.splice(0,0, "Islamabad", "Multan")
+// remove -- > 
+// console.log(cities.splice(-2, 2))
+
+
+// console.log(cities)
+
+// let studentsArr = ["hasan", "saleem", "kashif", "umair"]
+
+// console.log(studentsArr.slice(1,4))
+
+
+
+
+
+
+
+
+
