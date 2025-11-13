1a72e0c1960ed05a0f23870b29b8b7e5b49aa001
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-13 10:49:50 +0500

    Arrays Revision

---
+++ JavaScript/13-11-2025/app.js
@@ -0,0 +1,101 @@
+console.log("js running -->")
+
+
+
+var cleanestCity1 = "karachi"
+var cleanestCity2 = "lahore"
+var cleanestCity3 = "Islamabad"
+// console.log(cleanestCity1)
+
+// (), [], {}
+
+var cleanestCities = [cleanestCity1,cleanestCity2,cleanestCity3, "Peshawar", true, undefined, 45678, null, NaN, 12 , 78 , 85 , 78]
+
+
+// console.log(cleanestCities)
+cleanestCities[0] = "Multan"
+// console.log(cleanestCities)
+// console.log(cleanestCities[cleanestCities.length -2])
+
+
+
+// var name = "hasan"
+
+
+// name = "furqan"
+
+// console.log(name)
+
+
+// var name;  // declare
+
+// var name = "hasan"  // declare & initialize
+
+
+// name = "ahsan"   // reinitialize
+let animals = []
+
+// console.log(animals.length)
+animals[0] = "cat"
+animals[1] = "elephant"
+animals[2] = "tiger"
+animals[3] = {}
+// let removeElem = animals.shift()
+// let removeElem = animals.pop()
+// let removeElem = animals.unshift("horse", "lion","bear")
+// let removeElem = animals.push("horse", "lion","bear")
+
+// target index, kitny remove kerny hain
+// remove
+// let removeElem = animals.splice(1, 0,)
+// add -->
+// let removeElem = animals.splice(1, 0, "lion")
+// update -->
+// let removeElem = animals.splice(1, 1, "lion")
+console.log(animals)
+// console.log("return value ->",removeElem)
+
+// console.log(animals)
+
+
+let arr = [1, 2 ,3, 4,5,6,7,8,9,10]
+
+// console.log("return value",arr.splice(-2, 2))
+
+// slice kia kerta hai ?
+// slice array ke ander sy copy bana ker deta hai 
+
+// slice return kia kerta hai ? 
+
+// slice array ander sai copy element ko return kerta hai 
+
+// target index , 
+// let copyArrElem = arr.slice(4,7)  
+
+// let arrN = []
+
+// arrN.push(copyArrElem)
+// console.log("arrN -->",arrN[0][1])
+// // console.log("return value",arr.slice(4,7))
+
+
+// console.log("updated array -->",arr)
+
+
+
+
+// let newArr = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9,10]
+// ]
+
+// console.log(newArr[0][2])
+
+
+// literal notation --> []
+// object notation  --> new Array()
+
+// let stu = []
+// let stu = new Array()
+// console.log(stu)
