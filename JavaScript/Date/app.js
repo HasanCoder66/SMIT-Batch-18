7c7da6b9c689183eac5136fac3e3b6248329d508
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-20 12:58:36 +0500

    functions ch 35 to 37

---
+++ JavaScript/Date/app.js
@@ -0,0 +1,94 @@
+// console.log("js chl rhai hai")
+
+// let userAge = prompt("Enter your Age" ,"22 Aug 2003")
+
+// let userDOB = userAge
+
+// let dob = new Date(userDOB);
+// let currentDate = new Date()
+
+// let currentYear = currentDate.getFullYear()
+
+// console.log("current Year -->",currentYear - dob.getFullYear())
+// console.log(dob.getFullYear())
+
+
+
+
+
+
+
+
+
+// console.log("My Name is Hasan")
+
+// let arr = []
+// arr.push("Hasan")
+// console.log(arr)
+
+function tellTime() {
+var now = new Date();
+var theHr = now.getHours();
+var theMin = now.getMinutes();
+alert("Current time: "+ theHr + ":" + theMin);
+}
+
+// tellTime()
+
+
+function myAlert () {
+    alert("My name is hasan")
+}
+
+
+
+//   // parameters
+// function fullName (a, b, c) {
+// console.log(`My Name is ${c}`)
+// // console.log(arguments)
+// }
+ 
+// fullName("Uzair Shaikhani", "Hasan", "Abdullah")     //arguenment
+
+
+
+
+// function sum (n1, n2) {
+//     console.log( n2 )
+// }
+
+
+// sum()
+
+// if (2 + 2 == 4 + 4){
+//     console.log("right")
+// }
+
+
+// let n1 ;     
+
+// console.log(n1)
+
+
+
+function divide (n1, n2) {
+    let d = n1 / n2
+    console.log(d)
+
+    function sum () {
+        console.log("sum")
+    }
+    return sum
+    console.log("hasn")
+}
+
+
+let returnVal = divide(12, 2)
+console.log("return Value --> divide",returnVal())
+
+
+
+// let arr = [];
+// let returnVal = arr.push("Hasan")
+
+// console.log(arr.push("Hasan"))
