fce41fbd64880d96b78b2dd3f7e8e25bc4717d8e
hasanCoder66 <hasanashraf874@gmail.com>
2025-11-22 12:50:17 +0500

    functions revision | let vs const vs var | switch statements | local scope or global scope

---
+++ JavaScript/Functions/22-11-2025/app.js
@@ -0,0 +1,131 @@
+// console.log("js running -->")
+// let userName = "Ashas"
+// myName = "Isamil"
+// console.log(myName)
+
+// function getFullName (name){
+// console.log(name)
+// }
+
+// getFullName()    ///arguements
+
+
+//default parameters
+// function addNumber (a = 12, b = 45, c = 15) {
+//     // console.log(a + 22)
+//     console.log(c)
+// }
+
+// addNumber(22)
+
+
+
+
+// function foo () {
+//     var myName = "hasan"
+// }
+
+// foo()
+
+// let userName = "hasancoder66"
+// var firstName = "Hasan"
+
+// {
+//     let  myName = "Hasan"
+// }
+// console.log(myName)
+
+
+// hoisting 
+
+
+var declare;  //declare
+
+var declareAndInit = "12" //declare And Init
+var declareAndInit = "45" //declare And Init
+
+// console.log(declareAndInit)
+declareAndInit = "hasan"   //reinitialize
+
+let d;  //declare
+// let d = "declare hai bhai"
+
+// console.log(d)
+
+// d = "init"
+// d = "reinit"
+// console.log(d)
+// let 
+
+
+
+// const c = "cat";
+
+// // c = "horse"
+// console.log(c + "caterpeller")
+
+// if(false) {
+// var a = "a"
+// }else {
+// var b = "b"
+// }
+
+
+// console.log(b)
+
+
+
+// function xyz (a,b) {
+//     if(a == undefined || b == undefined) {
+//         return 
+//     }
+
+//     console.log(a("Enter Your Name"), b("Enter Your Age"))
+// }
+
+// xyz(prompt, prompt)
+
+
+// const fn = function () {
+// return "hasan"
+// }
+
+// console.log(fn())
+
+
+
+// let num  = 1;
+
+// if(num == 1) {
+//     console.log("one")
+// }else if(num == 2){
+// console.log("two")
+// }else if(num == 3){
+// console.log("three")
+// }else {
+//     console.log("apka milaya number durust nhi hai")
+// }
+
+
+
+// switch cases 
+ 
+
+let dice = Math.ceil(Math.random() * 8)
+// console.log(dice)
+
+switch (dice) {
+    case 1: console.log("one") 
+        break;
+    case 2: console.log("two")
+        break;
+    case 3: console.log("three")
+        break;
+    case 4: console.log("four")
+        break;
+    case 5: console.log("five")
+        break;
+    case 6: console.log("six")
+    break;
+    default : console.log("yaar sahi number to do")
+}
