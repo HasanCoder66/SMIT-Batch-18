b6a6596965c6534dbed16e8a55a77255f5f368ea
HasanCoder66 <hasanashraf874@gmail.com>
2025-11-27 13:07:55 +0500

    js ch 41 to 44 & document input

---
+++ JavaScript/Functions/27-11-2025/app.js
@@ -0,0 +1,91 @@
+// console.log("js running -->");
+
+// let para = "my name is hasan";
+
+// let charIndex = -1;
+// // console.log(para.indexOf("s"));
+
+// function myIndexOf(char) {
+//   for (let i = 0; i < para.length; i++) {
+//     if (para[i] == char) {
+//       charIndex = i;
+//       break;
+//     }
+//   }
+
+//   console.log(char);
+// }
+
+// myIndexOf("a");
+
+// console.log(charIndex);
+
+
+
+// loop kisy kehty thy ?
+
+
+// for kia mangta hai 
+// start  , end , change
+
+
+// initialization , condition, increment or decrement 
+
+
+// for(let i = 0 ; i < 10; i++) {
+//     //execution
+//     // console.log(i)
+//     for(let j = 0; j < 1000 ; j++) {
+//         console.log(i,j)
+//     }
+// }
+
+
+
+// while loop 
+// let i = 0;
+
+// while(i < 10){
+//     console.log(i)
+//     i++
+// }
+
+
+/*let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana"
+let para = "my name is fulana*/
+
+
+let emailInput = document.getElementById("email")
+let passInput = document.getElementById("password")
+
+// console.log(emailInput)
+// console.log(passInput)
+ 
+function submitHandler() {
+    console.log("Email -->",emailInput.value)
+    console.log("Password -->",passInput.value)
+    console.log("chl raha hon 0-->")
+}
+
+
+// alert("1")
+let i = 0; 
+
+do {
+    console.log(i)
+i++
+}while (i < 0)   // condition 
