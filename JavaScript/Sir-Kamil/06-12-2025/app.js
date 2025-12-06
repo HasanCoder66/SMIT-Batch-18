52ac24e93b8b264f775bac3f0ab3d8eaefb228d7
hasanCoder66 <hasanashraf874@gmail.com>
2025-12-06 11:01:01 +0500

    sir kamil class ch 45 to 48

---
+++ JavaScript/Sir-Kamil/06-12-2025/app.js
@@ -0,0 +1,46 @@
+// console.log("js running -->")
+// console.log(globalVar)
+
+
+// var globalVar = "a";
+// let localVar = "a";
+// console.log(localVar)
+
+
+
+// console.log(globalVar)
+
+
+// { var bottle;
+
+//     {
+//         let bottle = "Aquafina"
+        
+//     }
+//     console.log(bottle)
+// }
+
+// console.log(bottle)
+
+
+// function myClick() {
+//     console.log("mai chla -->")
+// }
+
+function onMouseEnter (e) {
+    // console.log(e)
+    src='https:/www.shutterstock.com/image-photo/sun-sets-behind-mountain-ranges-600nw-2479236003.jpg'
+}
+
+
+
+function focusFn(e) {
+console.log(e.style.backgroundColor = "blue")
+console.log(e.style.color = "white")
+// e.style
+}
+
+function myBlueFn(e) {
+    console.log(e)
+    e.value = "Abdullah hon main"
+}
